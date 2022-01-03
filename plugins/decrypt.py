8f0ebbcc50779da16d51b7bf00b571709e1e1468
Dzk <91489656+Dzancok@users.noreply.github.com>
2022-01-04 01:26:03 +0700

    Update decrypt.py

--- plugins/decrypt.py
+++ plugins/decrypt.py
@@ -6,10 +6,8 @@
 
 
 
-# print a GPL notice
-copyrightNotice = '''HCDecryptor, Copyright (C) 2020  HCTools\n
-This program comes with ABSOLUTELY NO WARRANTY.
-This is free software, and you are welcome to redistribute it under certain conditions.
+
+
 '''
 print(copyrightNotice)
 
